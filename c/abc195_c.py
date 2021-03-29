5c416d233f6a6ec80130419d2f9a3e636e32984b
wasshoy <404ari@gmail.com>
2021-03-29 15:43:08 +0900

    🐎回答追加

---
+++ c/abc195_c.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def input(): return sys.stdin.readline().strip()
+def I(): return int(input())
+def LI(): return list(map(int, input().split()))
+def IR(n): return [I() for i in range(n)]
+def LIR(n): return [LI() for i in range(n)]
+def SR(n): return [S() for i in range(n)]
+def S(): return input()
+def LS(): return input().split()
+
+
+INF = float('inf')
+
+n = I()
+ans = 0
+if n >= 1_000:
+    ans += n - 1_000 + 1
+if n >= 1_000_000:
+    ans += n - 1_000_000 + 1
+if n >= 1_000_000_000:
+    ans += n - 1_000_000_000 + 1
+if n >= 1_000_000_000_000:
+    ans += n - 1_000_000_000_000 + 1
+if n >= 1_000_000_000_000_000:
+    ans += n - 1_000_000_000_000_000 + 1
+print(ans)
