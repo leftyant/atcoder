c73f04eeb76e5061bb092982b50623f2bfbce571
wasshoy <404ari@gmail.com>
2021-04-09 23:03:14 +0900

    解答追加

--- a/abc074_a.java
+++ a/abc074_a.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-    public static int I(String s) {
-        return Integer.parseInt(s);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.next());
-        int a = Integer.parseInt(sc.next());
-        int ans = n * n - a;
-        System.out.println(ans);
-        sc.close();
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i=0; i < 15; i++) {
+            arr.add(sc.next());
+        }
+        Collections.sort(arr);
+        System.out.println(arr.get(6));
     }
 }
